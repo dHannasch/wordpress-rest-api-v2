70ee2802e5a8b4ca88ed590beeaeb8735bf1169e
David A. Hannasch <David.A.Hannasch@gmail.com>
2019-05-27 17:42:19 -0600

    test_get_posts

--- src/wordpress/api.py
+++ src/wordpress/api.py
@@ -1,6 +1,15 @@
 
 import configparser
 import urllib.parse
+import requests
+import typing
+
+"""
+When having authentication issues, we have options:
+https://2.python-requests.org/en/master/user/advanced/
+prepped.body = 'No, I want exactly this as the body.'
+del prepped.headers['Content-Type']
+"""
 
 class WordPressComCredential:
   def __init__(self):
@@ -16,12 +25,34 @@ class WordPressComCredential:
   def get_verification_url(self):
     return r'https://public-api.wordpress.com/oauth2/token-info?client_id={}&token={}'.format(self.client_id, self.get_encoded_token())
 
-def get_proxies():
+def get_blog_url() -> str:
+  configParser = configparser.ConfigParser()
+  configParser.read('comsecrets.ini')
+  return configParser['WordPressCom']['url']
+
+def get_proxies() -> dict:
   configParser = configparser.ConfigParser()
   configParser.read('comsecrets.ini')
   return {'http': configParser['Proxies']['http'], 'https': configParser['Proxies']['https']}
 
-def get_verify():
+def get_verify() -> str:
   configParser = configparser.ConfigParser()
   configParser.read('comsecrets.ini')
   return configParser['Proxies']['ssl_verify']
+
+def make_session() -> requests.Session:
+  """
+  This can be used as a context manager.
+  Any dictionaries that you pass to a request method will be merged with the session-level values that are set.
+  https://2.python-requests.org/en/master/api/#request-sessions
+  """
+  ret = requests.Session()
+  ret.cert = get_verify()
+  ret.proxies = get_proxies()
+  credential = WordPressComCredential()
+  ret.headers = {"Authorization": "Bearer " + credential.rawToken}
+  return ret
+
+def get_posts(session: requests.Session, url: str):
+  return requests.get(r'https://public-api.wordpress.com/wp/v2/sites/{}/posts'.format(url), proxies=session.proxies, verify=session.cert)
+
--- src/wordpress/test_wordpress.py
+++ src/wordpress/test_wordpress.py
@@ -3,6 +3,7 @@ import wordpress.api
 import configparser
 import requests
 
+
 def test_credential():
   credential = wordpress.api.WordPressComCredential()
   tokenValid = requests.get(credential.get_verification_url(), proxies=wordpress.api.get_proxies(), verify=wordpress.api.get_verify())
@@ -13,3 +14,28 @@ def test_credential():
   configParser.read('comsecrets.ini')
   assert tokenValidJSON['user_id'] == configParser['WordPressCom']['user_id']
   assert tokenValidJSON['blog_id'] == configParser['WordPressCom']['blog_id']
+
+def test_v1_me():
+  session = wordpress.api.make_session()
+  response = requests.get(r'https://public-api.wordpress.com/rest/v1/me/',
+                          headers=session.headers, proxies=session.proxies, verify=session.cert,
+                          json=dict())
+  assert response.status_code == 200
+  assert response.headers['Content-Type'] == 'application/json'
+  responseJSON = response.json()
+  assert responseJSON['verified']
+  configParser = configparser.ConfigParser()
+  configParser.read('comsecrets.ini')
+  assert responseJSON['ID'] == int(configParser['WordPressCom']['user_id'])
+  assert responseJSON['primary_blog'] == int(configParser['WordPressCom']['blog_id'])
+
+def test_get_posts():
+  response = wordpress.api.get_posts(wordpress.api.make_session(), wordpress.api.get_blog_url())
+  assert response.status_code == 200
+  assert response.headers['Content-Type'] == 'application/json; charset=UTF-8'
+  assert response.headers['Access-Control-Allow-Headers'] == 'Authorization, Content-Type'
+  # Access-Control-Allow-Headers sounds important but doesn't seem to matter:
+  assert 'Authorization' not in response.request.headers
+  assert 'Content-Type' not in response.request.headers
+  assert len(response.request.headers) >= 4
+  posts = response.json()
